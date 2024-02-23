d393f008494d63253209782331970ce0cb858cb2
shataev <vityashataev@gmail.com>
2024-02-23 10:48:04 +0700

    feat: add random to skewed rect, use canvas-sketch-util lib

--- sketches/sketch-skew.js
+++ sketches/sketch-skew.js
@@ -1,58 +1,83 @@
 const canvasSketch = require('canvas-sketch');
+const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
 };
 
 /**
  * Draw rectangle
  */
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
   let x, y, w, h, cx, cy;
+  let angle, rx, ry;
+  const num = 60;
+  const deg = 24;
+
+  let rects = [];
+
+  for (let i = 0; i < num; i ++) {
+    // Coordinates of the center of canvas
+    cx = random.range(0, width);
+    cy = random.range(0, height);
+
+    // Dimensions of rectangle
+    w = random.range(200, 600);
+    h = random.range(40, 200);
+
+    rects.push({
+      w,
+      h,
+      cx,
+      cy
+    })
+
+  }
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    // Coordinates of the center of canvas
-    cx = 0.5 * width;
-    cy = 0.5 * height;
+    rects.forEach(rect => {
 
-    // Dimensions of rectangle
-    w = 0.6 * width;
-    h = 0.1 * height;
+      const {w, h, cx, cy} = rect;
 
-    // Left top corner of the rectangle
-    x = -0.5 * w;
-    y = -0.5 * h
+      context.save();
 
-    context.save();
+      // Translate the context for placing the rectangle in the center
+      context.translate(cx, cy)
 
-    // Translate the context for placing the rectangle in the center
-    context.translate(cx, cy)
+      context.strokeStyle = 'blue';
 
-    context.strokeStyle = 'blue';
-    context.strokeRect(x, y, w, h);
+      drawSkewedRect({context, w, h, deg})
 
-    context.restore();
+      context.restore();
 
-    // Another method to draw rectangle: step by step
-    context.save();
-
-    // Change start point of drawing
-    cx = 0.5 * width - 0.5 * w;
-    cy = 0.5 * height - 0.5 * h + 2 * h;
-
-    context.translate(cx, cy)
-
-    context.beginPath();
-    context.moveTo(0, 0);
-    context.lineTo(w, 0 );
-    context.lineTo(w, h );
-    context.lineTo(0, h );
-    context.lineTo(0, 0 );
-    context.stroke();
+    })
   };
 };
+
+const drawSkewedRect = ({context, w = 600, h = 400, deg = 45}) => {
+  const angle = math.degToRad(deg);
+  const rx = Math.cos(angle) * w;
+  const ry = Math.sin(angle) * w;
+
+  context.save();
+
+  // Translate the context for placing the rectangle in the center
+  context.translate(-rx/2, -ry/2 - h/2)
+
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(rx, ry );
+  context.lineTo(rx, ry + h );
+  context.lineTo(0, h );
+  context.lineTo(0, 0 );
+  context.closePath();
+  context.stroke();
+
+  context.restore();
+}
 
 canvasSketch(sketch, settings);
