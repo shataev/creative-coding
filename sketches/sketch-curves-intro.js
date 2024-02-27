8d7cc483203fb261b9da0be8d43c5edeaa1e11a3
shataev <vityashataev@gmail.com>
2024-02-27 10:48:09 +0700

    feat: interaction with curve via mouse events

--- sketches/sketch-curves-intro.js
+++ sketches/sketch-curves-intro.js
@@ -1,15 +1,24 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
 
-const sketch = ({context}) => {
-  const points = [
-      new Point(200, 540),
-      new Point(340, 40, true),
-      new Point(880, 540),
+// We need this element for calculate cursor coordinates
+let elCanvas;
+let points;
+
+const sketch = ({canvas}) => {
+  points = [
+    new Point(200, 540),
+    new Point(340, 40, true),
+    new Point(880, 540),
   ]
+
+  elCanvas = canvas;
+  canvas.addEventListener('mousedown', onMouseDown)
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
@@ -22,6 +31,37 @@ const sketch = ({context}) => {
     points.forEach(point=>point.draw(context))
   };
 };
+
+
+const onMouseDown = (e) => {
+  window.addEventListener('mousemove', onMouseMove);
+  window.addEventListener('mouseup', onMouseUp);
+
+  const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+  const y = (e.offsetY / elCanvas.offsetHeight ) * elCanvas.height;
+
+  points.map(point => {
+    point.isDragging = point.hitTest(x, y);
+  })
+}
+
+const onMouseMove = (e) => {
+  const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+  const y = (e.offsetY / elCanvas.offsetHeight ) * elCanvas.height;
+
+  points.forEach(point => {
+    if (point.isDragging) {
+      point.x = x;
+      point.y = y;
+    }
+  })
+}
+
+const onMouseUp = () => {
+  window.removeEventListener('mousemove', onMouseMove);
+  window.removeEventListener('mouseup', onMouseUp);
+}
+
 
 class Point {
   constructor(x, y, control = false){
@@ -40,6 +80,15 @@ class Point {
     context.fill();
 
     context.restore();
+  }
+
+  hitTest(x, y) {
+    const dx = this.x - x;
+    const dy = this.y - y;
+
+    const dd = Math.sqrt(dx * dx + dy * dy);
+
+    return dd < 20;
   }
 }
 
