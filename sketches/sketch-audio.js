7fda5eb34a152cf3658b4480d480f19837558786
shataev <vityashataev@gmail.com>
2024-03-20 06:36:57 +0700

    feat: use mapRange instaed of average in audio-sketch

--- sketches/sketch-audio.js
+++ sketches/sketch-audio.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const {math} = require("canvas-sketch-util");
 
 const settings = {
   dimensions: [ 1080, 1080 ],
@@ -10,6 +11,7 @@ let manager;
 let audio;
 let audioContext, audioData, sourceNode, analyserNode;
 const sketch = () => {
+  const bins = [4, 12, 37];
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -21,16 +23,19 @@ const sketch = () => {
 
     analyserNode.getFloatFrequencyData(audioData);
 
-    const avg = getAverage(audioData);
+    bins.forEach(bin => {
+        const mapped = math.mapRange(audioData[bin], analyserNode.minDecibels, analyserNode.maxDecibels, 0, 1, true);
+        const radius = mapped * 300;
 
-    context.save();
-    context.translate(width/2, height/2);
-    context.lineWidth = 10;
+        context.save();
+        context.translate(width/2, height/2);
+        context.lineWidth = 10;
 
-    context.beginPath();
-    context.arc(0, 0,Math.abs(avg), 0, Math.PI * 2);
-    context.stroke();
-    context.restore();
+        context.beginPath();
+        context.arc(0, 0, radius, 0, Math.PI * 2);
+        context.stroke();
+        context.restore();
+    })
   };
 };
 
@@ -44,15 +49,10 @@ const createAudio = () => {
     sourceNode.connect(audioContext.destination);
 
     analyserNode = audioContext.createAnalyser();
+    analyserNode.smoothingTimeConstant = 0.95;
     sourceNode.connect(analyserNode);
 
     audioData = new Float32Array(analyserNode.frequencyBinCount);
-}
-
-const getAverage = (data) => {
-    const sum = data.reduce((acc, current) => acc + current);
-
-    return sum / data.length;
 }
 
 const start = async () => {
